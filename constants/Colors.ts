f62ad223cd71bb9ee518daa74d8860bcf9806c24
jcw1031 <jcw001031@gmail.com>
2024-04-16 02:11:37 +0900

    docs: add README.md

---
+++ constants/Colors.ts
@@ -0,0 +1,21 @@
+export const choenan = '#2C8E9A';
+export const singwan = '#E85239';
+export const yesan = '#E2981E';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    container: '',
+    tint: choenan,
+    tabIconDefault: '#ccc',
+    tabIconSelected: choenan,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: singwan,
+    tabIconDefault: '#ccc',
+    tabIconSelected: singwan,
+  },
+};
