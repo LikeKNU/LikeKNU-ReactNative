f461885f057bb95cadcb6f833e7f0050693f5dcb
jcw1031 <jcw001031@gmail.com>
2025-01-11 13:08:08 +0900

    feature: 채용소식 공지사항 탭 추가

--- constants/announcement.ts
+++ constants/announcement.ts
@@ -2,5 +2,6 @@ export const categories = {
   STUDENT_NEWS: { value: 'student-news', name: '학생소식' },
   DORMITORY: { value: 'dormitory', name: '생활관' },
   LIBRARY: { value: 'library', name: '도서관' },
+  RECRUITMENT: { value: 'recruitment-news', name: '채용소식' },
   // INTERNSHIP: { value: 'internship', name: '현장실습' }
 }
